f60a489a47104b9a262ca7e8ee78325a11e6aa50
Tayten Bennetsen <youremail@gmail.com>
2023-11-18 14:32:55 -0600

    yes

--- App.js
+++ App.js
@@ -1,62 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert, FlatList } from 'react-native';
 import axios from "axios";
 import { useState, useEffect } from 'react';
-
-export default function App() {
+const API_SERVER_URL = 'https://www.fruityvice.com/api/fruit/'; 
+const App = () => {
   const [FruitType, setFruitType] = useState('');
-  return (
-    <View style={styles.container}>
-      <Text>Fruit API</Text>
-      <TextInput
-        style={{height: 40}}
-        placeholder="Type your favorite fruit!"
-        onChange={setFruitType()}
-      />
-      <Text>{returnFruit(FruitType)}</Text>
-      <StatusBar style="auto" />
-    </View>
+  const renderItem = ({ item }) => (
+    <Text name={item.name} />
   );
+  const handleGenerateAPI = async () => {
+    if (!FruitType) {
+      Alert.alert('Error', 'Please enter a valid input.');
+      return;
+    }
+
+    try {
+      const response = await axios.get(API_SERVER_URL+FruitType);
+      console.log(response.data);
+      setFruitType(response.data);
+      Alert.alert('Success', 'API generated successfully.');
+
+    } catch (error) {
+      console.error('Error generating API:', error.message);
+      Alert.alert('Error', 'Failed to generate API.');
+    }
+  };
+  
+  console.log(FruitType)
+  return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Expo API Generator</Text>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+            margin: 10,
+            padding: 5,
+            width: 200,
+          }}
+          placeholder="Type your favorite fruit!"
+          onChangeText={(text) => setFruitType(text)}
+          value={FruitType}
+        />
+        
+        <Button title="Generate API" onPress={handleGenerateAPI} />
+        
+        
+        <FlatList
+          data={FruitType}
+          keyExtractor={(item, index) => index.toString()} 
+          renderItem={renderItem}
+        />
+      </View>
+    );
+  
   	//"Endpoint" URL where the data is located
   
 
-	// Set up the state to store the fetched data
-	const [data, setData] = useState([])
 
-	useEffect(() => {
-		// const fetchData = async () => {
-		// 	console.log("Making GET Request");
-		// 	try {
-		// 		// GET Request
-		// 		const response = await axios.get(URL)
-		// 				 .then((response) => {
-		// 				console.log(response)
-    //         setData(response.data)
-    //          })
-		// 		// Update the state with the fetched data
-					
-		// 	} catch (error) {
-		// 		// Handle any errors
-    //     console.error('Failed to fetch data: ', error);
-		// 	}
-		// }
-		// fetchData()
-
-    
-	}, []);
-  
-  function returnFruit (FruitType){
-    const URL = "https://www.fruityvice.com/api/fruit/" + FruitType
-
-    const response = axios.get(URL)
-						 .then((response) => {
-						console.log(response)
-				})
-    const data = response.data
-    const nutri = data["nutrition"]
-    console.log(nutri)
-    return nutri
-  }
 }
 const styles = StyleSheet.create({
   container: {
@@ -70,3 +72,4 @@ const styles = StyleSheet.create({
 
   }
 });
+export default App;
